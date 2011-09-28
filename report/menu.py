bcd481dee17f3927e3a878a3d357094f63527db5
leonardocsantoss <leonardocsantoss@gmail.com>
2011-09-28 13:43:35 -0300

    v1.1.1

--- report/menu.py
+++ report/menu.py
@@ -11,12 +11,11 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from models import ReportType
 from admin_tools.menu import items, Menu
+from django.db.models import Q
 
 
 class CustomMenu(Menu):
-    """
-    Custom Menu for Cofivi admin site.
-    """
+
     def __init__(self, **kwargs):
         Menu.__init__(self, **kwargs)
         self.children += [
@@ -31,18 +30,22 @@ class CustomMenu(Menu):
                 models=('django.contrib.*',)
             ),
         ]
-        
-        
+
+
     def init_with_context(self, context):
-        
-    
+
+
         request = context['request']
-        
+
         list_children = []
-        for report in ReportType.objects.filter(permissao__contains=request.user.groups.all()):
+        if request.user.is_superuser:
+            reports = ReportType.objects.all()
+        else:
+            reports = ReportType.objects.filter(Q(permissao__contains=request.user.groups.all()) | Q(permissao__isnull=True))
+        for report in reports:
             list_children.append(items.MenuItem(report.titulo, report.get_absolute_url()))
         self.children += [items.MenuItem(_(u'Relatórios'), children=list_children )]
-        
+
         return super(CustomMenu, self).init_with_context(context)
-        
-        
+
+
--- report/views.py
+++ report/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import admin
@@ -21,7 +21,7 @@ from datetime import datetime
 
 def get_field(modelo, campo):
     return modelo._meta.get_field(campo)
-    
+
 def get_value(obj, campo):
     return getattr(obj, campo)
 
@@ -34,17 +34,20 @@ def get_key(key):
 @login_required
 def report(request, report_id, template_name="report/report.html"):
     if request.user.is_staff:
-        rtype = get_object_or_404(ReportType, id=report_id, permissao__contains=request.user.groups.all())
+        rtype = get_object_or_404(ReportType, id=report_id)
+        if not request.user.is_superuser:
+            if rtype.permissao.count() and not len([val for val in  rtype.permissao.all() if val in request.user.groups.all()]):
+                raise Http404
         rtadmin = admin.site._registry[rtype.modelo.model_class()]
-        
+
         campos_dinamicos = {}
         for filtro in rtadmin.list_filter:
-            
+
             field = get_field(rtype.modelo.model_class(), filtro)
             if type(field) == models.BooleanField:
                 choices = (('1', _(u'Sim')), ('0', _(u'Não')), ('', _(u'Todos')))
                 campos_dinamicos[filtro] = forms.ChoiceField(required=False, choices=choices, label=_(field.verbose_name), widget=forms.RadioSelect())
-            
+
             elif type(field) == models.CharField:
                 if field.choices:
                     choices = field.choices
@@ -55,11 +58,11 @@ def report(request, report_id, template_name="report/report.html"):
                         choices.add((get_value(obj, filtro), get_value(obj, filtro)))
                     choices = list(choices)
                     campos_dinamicos[filtro] = forms.MultipleChoiceField(required=False, choices=choices, label=_(field.verbose_name), widget=forms.SelectMultiple())
-                    
+
             elif type(field) == models.DateField or type(field) == models.DateTimeField:
                 choices = (('q', _(u'Qualquer data')), ('h', _(u'Hoje')), ('m', _(u'Este mês')), ('a', _(u'Este ano')))
                 campos_dinamicos[filtro] = forms.ChoiceField(required=False, choices=choices, initial='q', label=_(field.verbose_name), widget=forms.RadioSelect())
-            
+
             elif type(field) == models.ForeignKey or type(field) == models.OneToOneField:
                 queryset = rtype.modelo.model_class()._meta.get_field(filtro).rel.to.objects.all()
                 campos_dinamicos[filtro] = forms.ModelMultipleChoiceField(required=False, queryset=queryset, label=_(field.verbose_name), widget=forms.SelectMultiple())
@@ -69,18 +72,18 @@ def report(request, report_id, template_name="report/report.html"):
                     choices.add((get_value(obj, filtro), get_value(obj, filtro)))
                 choices = list(choices)
                 campos_dinamicos[filtro] = forms.MultipleChoiceField(required=False, choices=choices, label=_(field.verbose_name), widget=forms.SelectMultiple())
-            
+
         FilterForm = type('', (forms.Form,), campos_dinamicos)
-        
+
         if request.method == "POST":
-            
+
             response = HttpResponse(mimetype='application/pdf')
             response['Content-Disposition'] = 'attachment; filename=%s.pdf' % smart_str(rtype.titulo).replace(" ", "_")
             queryset = rtype.modelo.model_class().objects.all()
 
             for field_name in rtadmin.list_filter:
                 field = get_field(rtype.modelo.model_class(), field_name)
-                
+
                 sql = {}
                 if type(field) == models.DateField or type(field) == models.DateTimeField:
                     try:
@@ -101,15 +104,15 @@ def report(request, report_id, template_name="report/report.html"):
                         if len(value) == 0: continue
                         sql["%s__in" % field_name] = value
                     except: pass
-                    
+
                 query = models.Q(**sql)
                 queryset = queryset.filter(query)
-                
+
             campos = rtype.campos.replace('[', '').replace(']', '').replace('u\'', '').replace('\'', '').replace(',', '').split(' ')
             list_report = []
             for c in campos:
                list_report.append(rtadmin.list_report[get_key(c)])
-                
+
             html = html_report_generic(smart_str(rtype.cabecalho), list_report, queryset)
             pdf = pisa.CreatePDF(html, response)
             return response
